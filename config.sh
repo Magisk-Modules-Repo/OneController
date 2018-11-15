8f751e3f017b82a590b0917db1975dcd30bbdb20
BuildTools <unconfigured@null.spigotmc.org>
2018-11-15 07:49:36 +0200

    Updated Template to 17000.

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     	1Controller		       "
   ui_print "*******************************"
 }
 
